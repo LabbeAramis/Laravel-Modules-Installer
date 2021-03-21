8b04577f76423bd277fa1bfe356c4c9910adae99
inter-oren <inter-oren@mail.ru>
2021-03-22 03:44:32 +0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Laravel Module Installer
+
+Based on Josh Brown's <a href="https://github.com/joshbrw/laravel-module-installer">package</a>.
---
+++ src/LaravelModuleInstaller.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace LabbeAramis\LaravelModuleInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+use LabbeAramis\LaravelModuleInstaller\Exceptions\LaravelModuleInstallerException;
+
+/**
+ * Class LaravelModuleInstaller
+ *
+ * @package LabbeAramis\LaravelModuleInstaller
+ */
+class LaravelModuleInstaller extends LibraryInstaller
+{
+  const DEFAULT_ROOT = "Modules";
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getInstallPath( PackageInterface $package )
+  {
+
+    return $this->getBaseInstallationPath() . '/' . $this->getModuleName( $package );
+  }
+
+  /**
+   * Get the base path that the module should be installed into.
+   * Defaults to Modules/ and can be overridden in the module's composer.json.
+   *
+   * @return string
+   */
+  protected function getBaseInstallationPath()
+  {
+
+    if ( !$this->composer || !$this->composer->getPackage() ) {
+      return self::DEFAULT_ROOT;
+    }
+
+    $extra = $this->composer->getPackage()->getExtra();
+
+    if ( !$extra || empty( $extra['module-dir'] ) ) {
+      return self::DEFAULT_ROOT;
+    }
+
+    return $extra['module-dir'];
+  }
+
+  /**
+   * Get the module name, i.e. "labbearamis/something-module" will be transformed into "Something"
+   *
+   * @param PackageInterface $package Compose Package Interface
+   *
+   * @return string Module Name
+   *
+   * @throws LaravelModuleInstallerException
+   */
+  protected function getModuleName( PackageInterface $package )
+  {
+
+    $name  = $package->getPrettyName();
+    $split = explode( "/", $name );
+
+    if ( count( $split ) !== 2 ) {
+      throw LaravelModuleInstallerException::fromInvalidPackage( $name );
+    }
+
+    $splitNameToUse = explode( "-", $split[1] );
+
+    if ( count( $splitNameToUse ) < 2 ) {
+      throw LaravelModuleInstallerException::fromInvalidPackage( $name );
+    }
+
+    if ( array_pop( $splitNameToUse ) !== 'module' ) {
+      throw LaravelModuleInstallerException::fromInvalidPackage( $name );
+    }
+
+    return implode( '', array_map( 'ucfirst', $splitNameToUse ) );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function supports( $packageType )
+  {
+
+    return 'laravel-module' === $packageType;
+  }
+}
